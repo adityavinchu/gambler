e84373859542badd1a76abe25a2055cb76c9eeaa
aditya <vinchuaditya@gmail.com>
2022-03-11 08:43:53 +0530

    stake of 100 and daily spend 1$

---
+++ gambler/gambler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gambler
+{
+    public class gambler
+    {
+        public void play()
+        {
+            double stake = 100;
+            int daily = 1;
+        }
+    }
+}
